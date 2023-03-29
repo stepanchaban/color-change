45b36cb428897ba83211617180b107b75cb139b3
Stepan Chaban <aziibro@yandex.ru>
2023-03-29 14:02:00 +0400

    Save hash and paste url

--- app.js
+++ app.js
@@ -36,21 +36,28 @@ function copyToClickBoard(text) {
   return navigator.clipboard.writeText(text)
 }
 
-function setRandomColors() {
-  const colors = [];
+function setRandomColors(isInitial) {
+  const colors = isInitial ? getColorsFromHash() : [];
 
-  cols.forEach((col) => {
+  cols.forEach((col, index) => {
     const text = col.querySelector('h2');
     const button = col.querySelector('button');
     const isLocked = col.querySelector('i').classList.contains('fa-lock'); 
-    const color = generateRandomColor();
-
+    
     if(isLocked) {
       colors.push(text.textContent)
       return
     }
 
-    colors.push(color);
+    const color = isInitial 
+    ? colors[index] 
+      ? colors[index]
+      : generateRandomColor()
+    : generateRandomColor();
+
+    if(!isInitial){
+      colors.push(color);
+    }
 
     text.textContent = color;
     col.style.background = color;
@@ -68,9 +75,21 @@ function setTextColor(text, color) {
 }
 
 function updateColorsHash(colors = []) {
-  document.location.hash = colors.map(col => {
+  document.location.hash = colors
+  .map((col) => {
     return col.toString().substring(1)
-  }).join('-');
+  })
+  .join('-');
 }
 
-setRandomColors();
+function getColorsFromHash() {
+  if (document.location.hash.length > 1) {
+    return document.location.hash
+    .substring(1)
+    .split('-')
+    .map((color) => '#' + color);
+  } 
+  return []
+}
+
+setRandomColors(true);
